20482a7d09100740cab14ed5fdf7a953681c40fa
이정원 <ejsi81@naver.com>
2021-11-15 17:11:42 +0900

    if~else 예제

---
+++ src/com/javaex/ex03/Ex02.java
@@ -0,0 +1,27 @@
+package com.javaex.ex03;
+
+import java.util.Scanner;
+
+public class Ex02 {
+	
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("점수: ");
+		int num = sc.nextInt();
+		
+		if(num>=60) {
+			//true일때만											
+			System.out.println("합격입니다.");
+		}else {
+			//false일때만
+			System.out.println("불합격입니다.");
+		}
+		
+		System.out.println("프로그램 종료");
+		sc.close();
+		}
+	}
+
+			
