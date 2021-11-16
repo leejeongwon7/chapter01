4826c26f4e7a696065efcd38c798e02792b9ffeb
이정원 <ejsi81@naver.com>
2021-11-16 11:11:57 +0900

    조건문에&&사용

--- src/com/javaex/ex03/Ex5_2.java
+++ src/com/javaex/ex03/Ex5_2.java
@@ -2,22 +2,22 @@ package com.javaex.ex03;
 
 import java.util.Scanner;
 
-public class Ex05 {
+public class Ex5_2 {
 
 	public static void main(String[] args) {
 		
 		Scanner sc = new Scanner(System.in);
 		System.out.println("점수를 입력하세요 :");
 		int point = sc.nextInt();
-		if (point>89) {
+		if (point>=90 ) {
 			System.out.println("A등급");
-		}else if(point>79) {
+		}else if(90>point && point>=80) {
 			System.out.println("B등급");
-		}else if(point>69){
+		}else if(80>point && point>=70){
 			System.out.println("C등급");
-		}else if(point>59){
+		}else if(70>point && point>=60){
 			System.out.println("D등급");
-		}else if(point<60){
+		}else{
 			System.out.println("F등급");
 		}
 		sc.close();
